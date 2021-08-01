5c4fa42c5610e1169e7b4c1d11da6f3e0fe495a1
Engku Ihsan <eihsan94@gmail.com>
2021-08-01 16:23:25 +0900

    connect db

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,28 @@
 const express = require('express');
 const router = express.Router();
 const { check, validationResult } = require('express-validator');
+const User = require('../../models/User')
+// @route       POST api/users
+// @desc        Register user
+// @access      Public
+router.get(
+    '/',
+    // check('name', 'Name is required').notEmpty(),
+    // check('email', 'Please include a valid email').isEmail(),
+    // check(
+    //     'password',
+    //     'Please enter a password with 6 or more characters'
+    // ).isLength({ min: 6 }),
+    async(req, res) => {
+        console.log(req);
+        const users = await User.find({})
+        res.json(users)
+        // const errors = validationResult(req);
+        // if (!errors.isEmpty()) {
+        //     return res.status(400).json({ errors: errors.array() });
+        // }
+    }
+);
 
 // @route       POST api/users
 // @desc        Register user
