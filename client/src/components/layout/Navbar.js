aad2250a990f36419eac2440f2bbfc75c68ce97b
Muhd Izzat <izart1994@gmail.com>
2021-08-05 11:13:56 +0800

    Second Commit

---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCode } from '@fortawesome/free-solid-svg-icons';
+
+export const Navbar = () => {
+    return (
+        <nav className="navbar bg-dark">
+            <h1>
+                <Link to="/"><FontAwesomeIcon icon={faCode} /> DevConnector</Link>
+            </h1>
+            <ul>
+                <li><Link to="/#">Developers</Link></li>
+                <li><Link to="/register">Register</Link></li>
+                <li><Link to="/login">Login</Link></li>
+            </ul>
+        </nav>
+    )
+}
