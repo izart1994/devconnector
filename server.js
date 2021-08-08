ad7eb6db4d231e9134ee7617e42d4ab6e0fb101f
Muhd Izzat <izart1994@gmail.com>
2021-08-08 18:25:36 +0800

    commit

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const express = require('express');
+import express from 'express';
 import connectDB from './config/db';
-const path = require('path');
+import path from 'path';
 
 const app = express();
 
